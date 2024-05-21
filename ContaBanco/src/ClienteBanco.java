88e5e63bd647ca53e01f3bb2363a7a87471833df
thayanezz <thayanesousatet@gmail.com>
2024-05-21 00:13:22 -0300

    Atualizando o projeto conta de banco

--- ContaBanco/src/ClienteBanco.java
+++ ContaBanco/src/ClienteBanco.java
@@ -3,8 +3,15 @@ public class ClienteBanco {
     public static void main(String [] args){
         ContaTerminal contaTerminal = new ContaTerminal();
 
-        System.out.println("Digite o número da sua conta: ");
-        contaTerminal.getNumero()
+        contaTerminal.getNumero();
+        contaTerminal.getAgencia();
+        contaTerminal.getNomeCliente();
+        contaTerminal.getSaldo();
+
+        System.out.println("Olá " + contaTerminal.nomeCliente
+                + ", obrigado por criar uma conta em nosso banco, sua agência é " + contaTerminal.agencia + ", conta "
+                + contaTerminal.numero + " e seu saldo " + contaTerminal.saldo +
+                " já está disponível para saque.");
         
     }
 }
--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,33 +1,48 @@
+import java.util.Scanner;
+
 public class ContaTerminal{
     
     int numero;
-    char agencia;
+    String agencia;
     String nomeCliente;
-    boolean saldo;
+    double saldo;
     
+    Scanner scanner = new Scanner(System.in);
+
     public int getNumero() {
+        System.out.println("Digite o número da conta: ");
+        this.numero = scanner.nextInt();
         return numero;
     }
     public void setNumero(int numero) {
         this.numero = numero;
     }
-    public char getAgencia() {
+    public String getAgencia() {
+        System.out.println("Digite o número da agência vinculada à conta: ");
+        this.agencia = scanner.next();
+
         return agencia;
     }
-    public void setAgencia(char agencia) {
+    public void setAgencia(String agencia) {
         this.agencia = agencia;
     }
     public String getNomeCliente() {
+        System.out.println("Digite o nome do cliente: ");
+        this.nomeCliente = scanner.next();
+
         return nomeCliente;
     }
     public void setNomeCliente(String nomeCliente) {
         this.nomeCliente = nomeCliente;
     }
-    public boolean isSaldo() {
+    public double getSaldo() {
+        System.out.println("Digite o saldo da conta: ");
+        this.saldo = scanner.nextDouble();
         return saldo;
     }
-    public void setSaldo(boolean saldo) {
+
+    public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
-    
 }
+
